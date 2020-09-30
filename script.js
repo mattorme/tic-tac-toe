3156828b4f2e4032910cf17049364b163735e435
Matt <ormematthewj@gmail.com>
2020-09-30 15:25:37 +1000

    check for draw

--- script.js
+++ script.js
@@ -84,6 +84,23 @@ function checkWin() {
   }
 }
 
+function checkDraw() {
+  if (checkWin) {
+  } else if (
+    (space1.textContent == playerOne || space1.textContent == playerTwo) &&
+    (space2.textContent == playerOne || space2.textContent == playerTwo) &&
+    (space3.textContent == playerOne || space3.textContent == playerTwo) &&
+    (space4.textContent == playerOne || space4.textContent == playerTwo) &&
+    (space5.textContent == playerOne || space5.textContent == playerTwo) &&
+    (space6.textContent == playerOne || space6.textContent == playerTwo) &&
+    (space7.textContent == playerOne || space7.textContent == playerTwo) &&
+    (space8.textContent == playerOne || space8.textContent == playerTwo) &&
+    (space9.textContent == playerOne || space9.textContent == playerTwo)
+  ) {
+    winnerMsg.textContent = `It's a draw`;
+  }
+}
+
 function resetGame() {
   space1.textContent = "";
   space2.textContent = "";
@@ -100,6 +117,7 @@ function resetGame() {
 function handleClick(event) {
   event.target.textContent = playerTurn;
   checkWin();
+  checkDraw();
   changeTurn();
 }
 
